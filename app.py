20745cc3db37869e058660a1a93e45824361eeea
kalibekk <75063043+kalibekk@users.noreply.github.com>
2021-08-24 12:59:10 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'logtest'
+print("111")
+mysql = MySQL(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        username = details['username1']
+        password = details['password1']
+        print("222")
+        cur = mysql.connection.cursor()
+        print("333")
+        cur.execute("INSERT INTO accounts(username, password) VALUES (%s, %s)", (username, password))
+        print("444")
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
